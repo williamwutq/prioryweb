4894fc546e90dc0ab1f0b09d988585f1cd565f0a
williamwutq <williamwutq@gmail.com>
2025-11-06 14:14:26 -0800

    Refactor buffer chain functions to improve memory management and add new functionalities for pushing, popping, and separating buffers.

--- compressor/_cp_chain.c
+++ compressor/_cp_chain.c
@@ -1,8 +1,9 @@
 #include "_cp_sys.h"
 #include "_cp_chain.h"
 #include <stddef.h>
+#include <stdlib.h>
 
-_cp_Buffer_ChainNode* _cp_buffer_chain_node_create(const _cp_Buffer* buffer) {
+_cp_Buffer_ChainNode* _cp_buffer_chain_node_create(_cp_Buffer* buffer) {
     _cp_Buffer_ChainNode* node = (_cp_Buffer_ChainNode*)malloc(sizeof(_cp_Buffer_ChainNode));
     if (node == NULL) {
         _cp_die("Failed to allocate memory for _cp_Buffer_ChainNode.");
@@ -47,9 +48,10 @@ void _cp_buffer_chain_free(_cp_Buffer_Chain* chain) {
     free(chain);
 }
 
-void _cp_buffer_chain_pusht(_cp_Buffer_Chain* chain, const _cp_Buffer* buffer) {
-    if (chain == NULL || buffer == NULL) {
-        _cp_die("Cannot push to a null buffer chain or push a null buffer.");
+void _cp_buffer_chain_pusht(_cp_Buffer_Chain* chain, _cp_Buffer* buffer) {
+    if (buffer == NULL) return;
+    if (chain == NULL) {
+        chain = _cp_buffer_chain_create();
     }
     _cp_Buffer_ChainNode* new_node = _cp_buffer_chain_node_create(buffer);
     if (chain->tail == NULL) {
@@ -65,9 +67,10 @@ void _cp_buffer_chain_pusht(_cp_Buffer_Chain* chain, const _cp_Buffer* buffer) {
     chain->total_size += buffer->size;
 }
 
-void _cp_buffer_chain_pushf(_cp_Buffer_Chain* chain, const _cp_Buffer* buffer) {
-    if (chain == NULL || buffer == NULL) {
-        _cp_die("Cannot push to a null buffer chain or push a null buffer.");
+void _cp_buffer_chain_pushf(_cp_Buffer_Chain* chain, _cp_Buffer* buffer) {
+    if (buffer == NULL) return;
+    if (chain == NULL) {
+        chain = _cp_buffer_chain_create();
     }
     _cp_Buffer_ChainNode* new_node = _cp_buffer_chain_node_create(buffer);
     if (chain->head == NULL) {
@@ -81,6 +84,36 @@ void _cp_buffer_chain_pushf(_cp_Buffer_Chain* chain, const _cp_Buffer* buffer) {
         chain->head = new_node;
     }
     chain->total_size += buffer->size;
+}
+
+void _cp_buffer_chain_push(_cp_Buffer_Chain* chain, _cp_Buffer* buffer, const size_t count) {
+    if (buffer == NULL) return;
+    if (chain == NULL) {
+        chain = _cp_buffer_chain_create();
+    }
+    if (count == 0) {
+        _cp_buffer_chain_pushf(chain, buffer);
+    }
+    size_t _chain_count = _cp_buffer_chain_count(chain);
+    if (count >= _chain_count) {
+        _cp_buffer_chain_pusht(chain, buffer);
+    } else {
+        // Insert at the specified index
+        _cp_Buffer_ChainNode* new_node = _cp_buffer_chain_node_create(buffer);
+        _cp_Buffer_ChainNode* current = chain->head;
+        for (size_t i = 0; i < count; i++) {
+            current = current->next;
+        }
+        new_node->prev = current->prev;
+        new_node->next = current;
+        if (current->prev != NULL) {
+            current->prev->next = new_node;
+        } else {
+            chain->head = new_node;
+        }
+        current->prev = new_node;
+        chain->total_size += buffer->size;
+    }
 }
 
 _cp_Buffer* _cp_buffer_chain_popt(_cp_Buffer_Chain* chain) {
@@ -123,6 +156,40 @@ _cp_Buffer* _cp_buffer_chain_popf(_cp_Buffer_Chain* chain) {
     return buffer;
 }
 
+_cp_Buffer* _cp_buffer_chain_pop(_cp_Buffer_Chain* chain, const size_t count) {
+    if (chain == NULL) {
+        return NULL;
+    }
+    size_t _chain_count = _cp_buffer_chain_count(chain);
+    if (count == 0) {
+        return _cp_buffer_chain_popf(chain);
+    }
+    if (count >= _chain_count - 1) {
+        return _cp_buffer_chain_popt(chain);
+    }
+    // Pop from the specified index
+    _cp_Buffer_ChainNode* current = chain->head;
+    for (size_t i = 0; i < count; i++) {
+        current = current->next;
+    }
+    _cp_Buffer* buffer = current->buffer;
+
+    if (current->prev != NULL) {
+        current->prev->next = current->next;
+    } else {
+        chain->head = current->next;
+    }
+    if (current->next != NULL) {
+        current->next->prev = current->prev;
+    } else {
+        chain->tail = current->prev;
+    }
+    chain->total_size -= buffer->size;
+
+    free(current);
+    return buffer;
+}
+
 void _cp_buffer_chain_iter(const _cp_Buffer_Chain* chain, const void (*func)(const _cp_Buffer* buffer, void* data), void* data) {
     if (chain == NULL || func == NULL) {
         return;
@@ -134,9 +201,9 @@ void _cp_buffer_chain_iter(const _cp_Buffer_Chain* chain, const void (*func)(con
     }
 }
 
-_cp_Buffer* _cp_buffer_chain_concat(_cp_Buffer_Chain* c1, _cp_Buffer_Chain* c2) {
+_cp_Buffer_Chain* _cp_buffer_chain_concat(_cp_Buffer_Chain* c1, _cp_Buffer_Chain* c2) {
     if (c1 == NULL || c2 == NULL) {
-        _cp_die("Cannot concatenate null buffer chains.");
+        return NULL;
     }
     _cp_Buffer_Chain* combined_chain = _cp_buffer_chain_create();
     _cp_Buffer_ChainNode* h1 = c1->head;
@@ -155,4 +222,109 @@ _cp_Buffer* _cp_buffer_chain_concat(_cp_Buffer_Chain* c1, _cp_Buffer_Chain* c2) 
         h2->prev = t1;
     }
     return combined_chain;
+}
+
+_cp_Buffer_Chain* _cp_buffer_chain_separate(_cp_Buffer_Chain* chain, size_t index) {
+    if (chain == NULL) {
+        return NULL; // No separation needed
+    }
+    if (index >= chain->total_size) {
+        return _cp_buffer_chain_create(); // No separation needed
+    }
+    _cp_Buffer_Chain* new_chain = _cp_buffer_chain_create();
+    size_t accumulated_size = 0;
+    _cp_Buffer_ChainNode* current = chain->head;
+    while (current != NULL) {
+        if (accumulated_size + current->buffer->size > index) {
+            break;
+        }
+        accumulated_size += current->buffer->size;
+        current = current->next;
+    }
+    if (current == NULL) {
+        return new_chain; // No separation needed
+    }
+    // Detach nodes from the original chain to the new chain
+    new_chain->head = current;
+    new_chain->tail = chain->tail;
+    new_chain->total_size = chain->total_size - accumulated_size;
+
+    if (current->prev != NULL) {
+        current->prev->next = NULL;
+        chain->tail = current->prev;
+    } else {
+        // The entire chain is moved
+        chain->head = NULL;
+        chain->tail = NULL;
+    }
+    current->prev = NULL;
+    chain->total_size = accumulated_size;
+
+    return new_chain;
+}
+
+_cp_Buffer_Chain* _cp_buffer_chain_split(_cp_Buffer_Chain* chain){
+    if (chain == NULL) {
+        return NULL; // No separation needed
+    }
+    _cp_Buffer_Chain* new_chain = _cp_buffer_chain_create();
+    size_t accumulated_size = 0;
+    _cp_Buffer_ChainNode* current = chain->head;
+    while (current != NULL) {
+        if (accumulated_size + current->buffer->size > chain->total_size / 2) {
+            break;
+        }
+        accumulated_size += current->buffer->size;
+        current = current->next;
+    }
+    if (current == NULL) {
+        return new_chain; // No separation needed
+    }
+    // Detach nodes from the original chain to the new chain
+    new_chain->head = current;
+    new_chain->tail = chain->tail;
+    new_chain->total_size = chain->total_size - accumulated_size;
+
+    if (current->prev != NULL) {
+        current->prev->next = NULL;
+        chain->tail = current->prev;
+    } else {
+        // The entire chain is moved
+        chain->head = NULL;
+        chain->tail = NULL;
+    }
+    current->prev = NULL;
+    chain->total_size = accumulated_size;
+
+    return new_chain;
+}
+
+_cp_Buffer* _cp_buffer_chain_embuffer(_cp_Buffer_Chain* chain) {
+    if (chain == NULL || chain->total_size == 0) {
+        return _cp_buffer_create(); // Return an empty buffer
+    }
+    _cp_Buffer* combined_buffer = _cp_buffer_create_cap(chain->total_size);
+    if (combined_buffer == NULL) {
+        _cp_die("Failed to create combined buffer.");
+    }
+    _cp_Buffer_ChainNode* current = chain->head;
+    while (current != NULL) {
+        _cp_buffer_concat_inplace(combined_buffer, current->buffer);
+        current = current->next;
+    }
+    _cp_buffer_chain_free(chain);
+    return combined_buffer;
+}
+
+size_t _cp_buffer_chain_count(const _cp_Buffer_Chain* chain) {
+    if (chain == NULL) {
+        return 0;
+    }
+    size_t count = 0;
+    _cp_Buffer_ChainNode* current = chain->head;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
 }
--- compressor/_cp_chain.h
+++ compressor/_cp_chain.h
@@ -28,10 +28,12 @@ typedef struct {
 /**
  * Creates a new _cp_Buffer_ChainNode containing the specified buffer.
  * The node is not linked to any other nodes upon creation.
+ * Although buffer is not modified, this function takes a non-const pointer to allow ownership transfer.
+ * This means do not pass in buffers that cannot be freed or are constructed by const macros.
  * @param buffer The buffer to store in the node.
  * @return A pointer to the newly created _cp_Buffer_ChainNode.
  */
-_cp_Buffer_ChainNode* _cp_buffer_chain_node_create(const _cp_Buffer* buffer);
+_cp_Buffer_ChainNode* _cp_buffer_chain_node_create(_cp_Buffer* buffer);
 /**
  * Frees the memory allocated for a _cp_Buffer_ChainNode.
  * The node itself is freed with its buffer.
@@ -58,7 +60,7 @@ void _cp_buffer_chain_free(_cp_Buffer_Chain* chain);
  * @param chain The buffer chain to append to.
  * @param buffer The buffer to append.
  */
-void _cp_buffer_chain_pusht(_cp_Buffer_Chain* chain, const _cp_Buffer* buffer);
+void _cp_buffer_chain_pusht(_cp_Buffer_Chain* chain, _cp_Buffer* buffer);
 /**
  * Push a buffer to the front of the buffer chain.
  * A new node is created to hold the buffer and linked to the front of the chain.
@@ -66,7 +68,21 @@ void _cp_buffer_chain_pusht(_cp_Buffer_Chain* chain, const _cp_Buffer* buffer);
  * @param chain The buffer chain to prepend to.
  * @param buffer The buffer to prepend.
  */
-void _cp_buffer_chain_pushf(_cp_Buffer_Chain* chain, const _cp_Buffer* buffer);
+void _cp_buffer_chain_pushf(_cp_Buffer_Chain* chain, _cp_Buffer* buffer);
+/**
+ * Push a buffer to the specified index in the buffer chain.
+ * If the index is 0, the buffer is pushed to the front.
+ * If the index is greater than or equal to the current count, the buffer is pushed to the end.
+ * Otherwise, the buffer is inserted at the specified index.
+ * The total size of the chain is updated accordingly.
+ * 
+ * In most cases, use this function is not necessary. For binary split, use _cp_buffer_chain_split and _cp_buffer_chain_concat instead, 
+ * as they are much more efficient.
+ * @param chain The buffer chain to modify.
+ * @param buffer The buffer to insert.
+ * @param count The index at which to insert the buffer. THIS INDEX IS NOT CHARACTER OR BYTE BASED, BUT NODE BASED.
+ */
+void _cp_buffer_chain_push(_cp_Buffer_Chain* chain, _cp_Buffer* buffer, const size_t count);
 /**
  * Pop a buffer from the end of the buffer chain.
  * The node containing the buffer is removed from the chain and freed.
@@ -84,6 +100,18 @@ _cp_Buffer* _cp_buffer_chain_popt(_cp_Buffer_Chain* chain);
  */
 _cp_Buffer* _cp_buffer_chain_popf(_cp_Buffer_Chain* chain);
 /**
+ * Pop a buffer from the specified index in the buffer chain.
+ * The node containing the buffer is removed from the chain and freed.
+ * The total size of the chain is updated accordingly.
+ * 
+ * In most cases, use this function is not necessary. For binary split, use _cp_buffer_chain_split and _cp_buffer_chain_concat instead, 
+ * as they are much more efficient.
+ * @param chain The buffer chain to pop from.
+ * @param count The index from which to pop the buffer. THIS INDEX IS NOT CHARACTER OR BYTE BASED, BUT NODE BASED.
+ * @return A pointer to the buffer that was removed from the specified index of the chain.
+ */
+_cp_Buffer* _cp_buffer_chain_pop(_cp_Buffer_Chain* chain, const size_t count);
+/**
  * Iterates over each buffer in the buffer chain and applies the specified function that reads the buffer as a constant.
  * The function is called with each buffer and the provided data pointer.
  * @param chain The buffer chain to iterate over.
@@ -91,5 +119,47 @@ _cp_Buffer* _cp_buffer_chain_popf(_cp_Buffer_Chain* chain);
  * @param data A pointer to user-defined data to pass to the function. This can be anything of any type.
  */
 void _cp_buffer_chain_iter(const _cp_Buffer_Chain* chain, const void (*func)(const _cp_Buffer* buffer, void* data), void* data);
+/**
+ * Concatenates two buffer chains into a new buffer chain.
+ * The original chains are freed, but their buffers are preserved in the new chain.
+ * @param c1 The first buffer chain.
+ * @param c2 The second buffer chain.
+ * @return A pointer to the newly created concatenated buffer chain.
+ */
+_cp_Buffer_Chain* _cp_buffer_chain_concat(_cp_Buffer_Chain* c1, _cp_Buffer_Chain* c2);
+/**
+ * Separates a buffer chain into two chains at the specified index.
+ * The original chain is modified to contain only the buffers before the index.
+ * A new chain is created containing the buffers from the index onward.
+ * @param chain The original buffer chain to separate.
+ * @param index The index at which to separate the chain.
+ * @return A pointer to the newly created buffer chain containing the separated buffers.
+ */
+_cp_Buffer_Chain* _cp_buffer_chain_separate(_cp_Buffer_Chain* chain, size_t index);
+/**
+ * Splits the buffer chain into two halves.
+ * The original chain is modified to contain the first half of the buffers.
+ * A new chain is created containing the second half of the buffers.
+ * If the chain has an odd number of buffers, the extra buffer goes to the first half.
+ * @param chain The original buffer chain to split.
+ * @return A pointer to the newly created buffer chain containing the second half of the buffers.
+ */
+_cp_Buffer_Chain* _cp_buffer_chain_split(_cp_Buffer_Chain* chain);
+/**
+ * Embuffers the entire buffer chain into a single contiguous _cp_Buffer.
+ * All buffers in the chain are copied into a new buffer.
+ * The original chain is freed after embuffering, so is the buffers within it, except in some cases
+ * when some buffers are reused directly to avoid unnecessary copying.
+ * @param chain The buffer chain to embuffer.
+ * @return A pointer to the newly created _cp_Buffer containing the combined data.
+ */
+_cp_Buffer* _cp_buffer_chain_embuffer(_cp_Buffer_Chain* chain);
+/**
+ * Counts the number of buffers in the buffer chain. This number is not the total size in bytes,
+ * but the count of individual _cp_Buffer objects contained within the chain.
+ * @param chain The buffer chain to count.
+ * @return The number of buffers in the chain.
+ */
+size_t _cp_buffer_chain_count(const _cp_Buffer_Chain* chain);
 
 #endif // _CP_CHAIN_H
