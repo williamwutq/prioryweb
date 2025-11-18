eec14b7a82a16d78c7f66ef983335f5e4998d2fa
williamwutq <williamwutq@gmail.com>
2025-11-18 00:40:19 -0800

    Add function signatures

--- compressor/_cp_base64.h
+++ compressor/_cp_base64.h
@@ -27,4 +27,21 @@ void _cp_base64_write_6_bits(_cp_Buffer* buf, const size_t offset, const size_t 
  */
 byte _cp_base64_read_6_bits(const _cp_Buffer* buf, size_t offset, size_t index);
 
+/**
+ * Decodes a base64 encoded buffer back into a string and append to output buffer.
+ * @param buf The buffer containing base64 encoded data.
+ * @param offset The byte offset in the buffer.
+ * @param out The output buffer to write decoded characters into.
+ */
+void _cp_base64_decode(const _cp_Buffer* buf, const size_t offset, _cp_Buffer* out);
+
+/**
+ * Encodes a string into base64 format and writes to buffer.
+ * @param buf The buffer to write to.
+ * @param str Buffer containing. Null termination is followed, which means if str contains null character,
+ *            encoding stops there, even if length indicates more characters.
+ * @param offset The byte offset in the buffer.
+ */
+void _cp_base64_encode(_cp_Buffer* buf, const _cp_Buffer* str, const size_t offset);
+
 #endif // _CP_BASE64_H_
